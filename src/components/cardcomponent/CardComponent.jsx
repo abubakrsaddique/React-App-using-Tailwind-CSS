8fbc4b1be8aa16f7ee42524303ae86d19ed8dfac
M Abubakar Saddique <abubakarsaddique786.tsk@gmail.com>
2024-06-25 23:19:39 +0500

    8th commit

---
+++ src/components/cardcomponent/CardComponent.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import ImageSrc from "../../images/card2.png";
+
+const CardComponent = () => {
+  return (
+    <div className="mt--16 ml-[34px]">
+      <div className="flex flex-row justify-center gap-12 mr-96 mt-0">
+        <div
+          className="ml-96 -mt-16 w-auto  flex justify-center"
+          data-aos="fade-right"
+        >
+          <div className="bg-white rounded-full shadow-lg h-[100px] px-6 flex items-center">
+            <h1 className="text-base font-medium pl-2">
+              72k+ Happy <br />
+              Customers
+            </h1>
+          </div>
+        </div>
+        <div
+          className="-ml-8 -mt-16  w-auto flex justify-center"
+          data-aos="fade-left"
+        >
+          <div className="bg-white rounded-full shadow-lg h-[100px] px-6 flex items-center">
+            <img src={ImageSrc} className="h-auto max-w-full" alt="" />
+            <h1 className="text-base font-medium pl-2">
+              200+ New <br />
+              Listings Everyday!
+            </h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CardComponent;
