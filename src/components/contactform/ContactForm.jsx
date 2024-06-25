e73925fee3004782c47c9d57cb9044a172ceb35a
M Abubakar Saddique <abubakarsaddique786.tsk@gmail.com>
2024-06-25 13:20:18 +0500

    6th commit

---
+++ src/components/contactform/ContactForm.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+const ContactForm = () => {
+  return (
+    <div className="flex justify-center items-center ml-[52%] mt-[-208px]">
+      <div className="bg-white rounded-[30px] shadow-[10px_30px_50px_rgba(0,0,0,0.05)] h-auto max-w-[420px] w-[420px] p-[29px_20px]">
+        <div className="flex flex-col ">
+          <h1 className="text-[16px] text-[#3a0ca3] font-medium text-center">
+            Contact Us Now
+          </h1>
+          <div className="flex justify-center mt-5">
+            <div className="w-[344px] rounded-full border-[#3a0ca3] border-t-2"></div>
+          </div>
+          <div className="border-t border-gray-400 w-[110.5%] mt-[1px] ml-[-20px]"></div>
+          <div className="flex flex-col space-y-6 w-[90%] ml-5 mt-[40px]">
+            <input
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Full Name"
+              required
+              className="bg-[hsla(0,0%,83%,0.2)] border-none rounded-[20px] h-[59px] pl-[20px] pr-[25px] focus:outline-none"
+            />
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Email Address"
+              required
+              className="bg-[hsla(0,0%,83%,0.2)] border-none rounded-[20px] h-[59px] pl-[20px] pr-[25px] focus:outline-none"
+            />
+            <textarea
+              id="message"
+              name="message"
+              placeholder="Message"
+              required
+              className="bg-[hsla(0,0%,83%,0.2)] border-none rounded-[20px] p-[25px_20px] resize-none h-[150px] focus:outline-none"
+            ></textarea>
+            <button
+              type="submit"
+              className="bg-[#3a0ca3] text-white rounded-[50px] text-[16px] font-medium h-[60px] w-full hover:bg-[#6d28d9]"
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactForm;
--- src/components/realestatesection/RealEstateSection.jsx
+++ src/components/realestatesection/RealEstateSection.jsx
@@ -1,21 +1,25 @@
 import React from "react";
+import ContactForm from "../contactform/ContactForm";
 
 const RealEstateSection = () => {
   return (
     <div className="bg-gradient-to-b from-transparent to-blue-300 rounded-b-[800px] h-[800px] w-full p-0 m-0">
       <div className="mt-[200px] ml-[85px] w-[40%]">
         <div className="flex flex-col items-start space-y-5">
-          <h1 className="text-blue-600 text-[19px] font-medium tracking-[2.91px]">
+          <h1 className="text-blue-600 text-xl font-medium tracking-[2.91px]">
             REAL ESTATE
           </h1>
-          <h2 className="text-black text-[57.3px] font-semibold leading-[65px]">
+          <h2 className="text-black text-6xl font-semibold leading-[65px]">
             Find a perfect home you love..!
           </h2>
-          <p className="text-[16px] w-full">
+          <p className="text-base text-gray-400 w-full">
             Etiam eget elementum elit. Aenean dignissim dapibus vestibulum.
             Integer a dolor eu sapien sodales vulputate ac in purus.
           </p>
         </div>
+      </div>
+      <div>
+        <ContactForm />
       </div>
     </div>
   );
